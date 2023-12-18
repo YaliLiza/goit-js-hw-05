f8045a51937bc4ea53861e1dfa991bb9c5702a60
YaliLiza <yaliliza2007@gmail.com>
2023-12-18 20:34:26 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,7 @@
- function makeTransaction(quantity, pricePerDroid) {
+"use strict";
+
+
+function makeTransaction(quantity, pricePerDroid) {
     const totalPrice = quantity * pricePerDroid;
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
