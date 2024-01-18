6ec4ad00e552f0b41ce9e955f51c1a7811554136
YaliLiza <yaliliza2007@gmail.com>
2024-01-18 13:40:21 +0200

    .

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,16 @@
- function slugify(title) {
-  const slug = title.split(" ");
-  return slug.join("-").toLowerCase();
+ function isEnoughCapacity(products, containerSize) {
+  const productCopy = Object.values(products);
+  let totalResult = 0;
+  for (let i of productCopy) {
+    totalResult += i;
+  }
+  return totalResult <= containerSize;
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
+
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
+
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)); // true
+
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)); // false
